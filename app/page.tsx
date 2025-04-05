74ea5ea088f2f01a7227b0609560b30546cd58f9
Juan Santos <juan.moreira62@outlook.com>
2025-04-04 23:58:51 -0300

    creating appointment page

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
+            <p className="copyright mt-10 py-12">
               © 2024 CarePulse
             </p>
             <Link href="/?admin=true" className="text-green-500">
